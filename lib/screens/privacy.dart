5abd2c1402d68ca744a8703b16aa0308ba31e914
nemoryoliver <nemoryoliver@gmail.com>
2022-05-08 16:44:09 +0800

    Github Pages

--- lib/screens/privacy.dart
+++ lib/screens/privacy.dart
@@ -17,19 +17,18 @@ Information Collection and Use
 
 The app does use third-party services but never any personally identifiable information.
 
-Link to the privacy policy of third-party service providers used by the app
+Third Party Service Providers
 
 Google Analytics for Firebase
 Firebase Crashlytics
-Log Data
 
-We want to inform you that whenever you use our Service, in a case of an error in the app we collect data and information (through third-party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing our Service, the time and date of your use of the Service, and other statistics.
+We want to inform you that whenever you use our Service, in a case of an error in the app we collect data and information (through third-party products) on your phone called Log Data. This Log Data may include information such as your device name, operating system version, the configuration of the app when utilizing our Service, the time and date of your use of the Service, and other statistics.
 
 Cookies
 
 Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory.
 
-This Service does not use these “cookies” explicitly. However, the app may use third-party code and libraries that use “cookies” to improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.
+This Service does not use these “cookies” explicitly.
 
 Service Providers
 
@@ -54,7 +53,7 @@ Changes to This Privacy Policy
 
 We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page.
 
-This policy is effective as of 2022-04-27
+This policy is effective as of 2022-05-03
 
 Contact Us
 
