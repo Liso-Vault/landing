bdc21dbaa2e35597215c05d665db0017c17cf3f9
nemoryoliver <nemoryoliver@gmail.com>
2022-05-18 20:24:05 +0800

    Updated links Updated description

--- lib/core/utils/globals.dart
+++ lib/core/utils/globals.dart
@@ -6,12 +6,12 @@ const kColor = Color(0xff02f297);
 // TEXTS
 const kName = 'Liso';
 
-const kBetaSubText = 'v0.3.1+17 - May 17, 2022';
+const kBetaSubText = 'v0.3.2+18 - May 18, 2022';
 
 const kDescription = """
 An open-source data management application that focuses mainly on securely storing private information and files.
 
-Liso uses a powerful decentralized network of computers (Sia) to route your files, making it impossible for anyone to leak or sell your data. And with Sia's protocols, your files are broken into 30 pieces, each of which is encrypted and only you can decrypt them.
+Liso uses a powerful decentralized network of computers to route your files, making it impossible for anyone to leak or sell your data. You have the option to choose your provider: IPFS, Sia, Storj, and Skynet.
 """;
 
 const kInviteText =
@@ -32,9 +32,9 @@ const kIOSUrl = 'https://appdistribution.firebase.dev/i/af79efd9bce30e26';
 const kAndroidUrl =
     'https://play.google.com/store/apps/details?id=com.liso.app';
 const kMacOSUrl =
-    'https://github.com/Liso-Vault/app/releases/download/0.3.1%2B17/Liso_v0.3.1+17-MacOS.dmg';
+    'https://github.com/Liso-Vault/app/releases/download/0.3.2%2B18/Liso_v0.3.2+18-MacOS.dmg';
 const kWindowsUrl =
-    'https://github.com/Liso-Vault/app/releases/download/0.3.1%2B17/Liso_v0.3.1+17-Windows.exe';
+    'https://github.com/Liso-Vault/app/releases/download/0.3.2%2B18/Liso_v0.3.2+18-Windows.exe';
 
 // DEVELOPER
 const kDeveloperName = 'Stackwares';
